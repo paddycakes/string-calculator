a0b540c2dff9caf9fa47a2e14bb2d8185ce3895c
Patrick Gallagher <paddy.gallagher@gmail.com>
2014-10-18 12:29:14 +0100

    Refactor to make the Calculator instance creation the responsibility of the @Before setup rather than duplicating in each test method

--- src/test/java/com/tools/CalculatorTest.java
+++ src/test/java/com/tools/CalculatorTest.java
@@ -3,24 +3,27 @@ package com.tools;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
 
 	private static final String EMPTY_STRING = "";
-
+	
+	private Calculator calculator;
+	
+	@Before
+	public void setUp() {
+		calculator = new Calculator();
+	}
 
 	@Test
 	public void should_return_zero_for_an_empty_string() {
-		Calculator calculator = new Calculator();
-		
 		assertThat(calculator.add(EMPTY_STRING), is(0));
 	}
 	
 	@Test
 	public void should_return_number_for_single_number_string() {
-		Calculator calculator = new Calculator();
-		
 		assertThat(calculator.add("1"), is(1));
 	}
 
