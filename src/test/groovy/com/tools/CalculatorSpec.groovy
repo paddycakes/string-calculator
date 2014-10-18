e93a1e5ecc9638b1715cfdb734a0b37ff607ab40
Patrick Gallagher <paddy.gallagher@gmail.com>
2014-10-18 15:00:02 +0100

    Adding data-driven Spock tests for single number and two number strings

--- src/test/groovy/com/tools/CalculatorSpec.groovy
+++ src/test/groovy/com/tools/CalculatorSpec.groovy
@@ -15,8 +15,31 @@ class CalculatorSpec extends Specification {
 	def "should return 0 for an empty string"() {
 		when:
 			def sum = calculator.add(EMPTY_STRING)
+			
 		then:
 			sum == 0
+	}
+	
+	def "should return number for single number string input"(String input, int sum) {
+		expect:
+			calculator.add(input) == sum
+
+		where:
+			input | sum
+			 "1"  |  1 
+			 "2"  |  2
+			 "3"  |  3
+	}
+	
+	def "should return sum for string with two numbers delimited by comma"(String input, int sum) {
+		expect:
+			calculator.add(input) == sum
+
+		where:
+			input | sum
+			"1,2" | 3
+			"2,1" | 3
+			"0,15"| 15
 	}
 
 }
