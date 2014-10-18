2a6c350caeafbaccefd1fef5f800d9fbee85375f
Patrick Gallagher <paddy.gallagher@gmail.com>
2014-10-18 13:04:03 +0100

    Refactor to make each processing step a separate method

--- src/main/java/com/tools/Calculator.java
+++ src/main/java/com/tools/Calculator.java
@@ -14,21 +14,29 @@ public class Calculator {
 			return 0;
 		} else {
 			List<Integer> operands = parseOperands(input);
-			int result = 0;
-			for (Integer operand : operands) {
-				result += operand;
-			}
-			return result;
+			return addOperands(operands);
 		}
+	}
+
+	private int addOperands(List<Integer> operands) {
+		int result = 0;
+		for (Integer operand : operands) {
+			result += operand;
+		}
+		return result;
 	}
 
 	private List<Integer> parseOperands(String input) {
 		String[] numbers = input.split(COMMA_DELIMITER);
 		List<Integer> operands = new ArrayList<>(numbers.length);
 		for (int i=0; i<numbers.length; i++) {
-			operands.add(parseInt(numbers[i]));
+			operands.add(convertToInt(numbers[i]));
 		}
 		return operands;
+	}
+
+	private int convertToInt(String number) {
+		return parseInt(number);
 	}
 
 }
